cba9f29571f30b375416c4047bd27a6fa6335534
Anthony Tarlao <anthony.tarlao_ext@sonepar.com>
2022-10-31 09:58:55 -0400

    limit role to admin fetch pieces to write JSON

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs/promises')
+
 module.exports = {
   extend: '@apostrophecms/piece-type',
   options: {
@@ -9,6 +11,8 @@ module.exports = {
     },
     seoFields: false,
     openGraph: false,
+    editRole: 'admin',
+    publishRole: 'admin',
   },
   fields: {
     add: {
@@ -53,12 +57,73 @@ module.exports = {
   },
   methods(self) {
     return {
-      getNamespaces(req) {
+      getNamespaces() {
         return (
           Object.keys(self.apos.i18n.namespaces).map(ns => ({ label: ns, value: ns })) || [
             { label: 'default', value: 'default' },
           ]
         )
+      },
+
+      formatPieces(pieces) {
+        return pieces.reduce(
+          (acc, cur) => ({
+            ...acc,
+            [cur.key]: {
+              singular: cur.valueSingular,
+              plural: cur.valuePlural,
+              zero: cur.valueZero,
+            },
+          }),
+          {},
+        )
+      },
+
+      async writeFile(locale) {
+        try {
+          const localesDir = self.apos.i18n.options.directory
+          const file = localesDir + '/' + locale + '.json'
+          await fs.ensureFile(file)
+
+          const req = self.apos.tasks.getAnonReq()
+          const pieces = await self
+            .find(
+              req,
+              { published: true, aposLocale: locale },
+              {
+                title: 1,
+                namespace: 1, // => create one file per namespace?
+                valueSingular: 1,
+                valuePlural: 1,
+                valueZero: 1,
+              },
+            )
+            .toArray()
+          const translations = self.formatPieces(pieces)
+          fs.writeJson(file, translations, { spaces: 2 })
+        } catch (error) {
+          console.error(error.message)
+        }
+      },
+    }
+  },
+  tasks(self) {
+    return {
+      generateOne: {
+        usage: 'Write JSON file',
+        async task(argv) {
+          if (argv.locale) {
+            await self.writeFile(argv.locale)
+          }
+        },
+      },
+      generateAll: {
+        usage: 'Write JSON files',
+        async task(argv) {
+          for (const locale of Object.keys(self.apos.i18n.locales)) {
+            await self.writeFile(locale)
+          }
+        },
       },
     }
   },
